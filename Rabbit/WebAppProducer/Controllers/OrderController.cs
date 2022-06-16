151139182e3cf47899d135b3fa6248ec75ff1487
igorthebarros <1igorhbarros@gmail.com>
2022-06-16 19:35:38 -0300

    :sparkles: ASP.Net Core API Publisher Rabbit

---
+++ Rabbit/WebAppProducer/Controllers/OrderController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+using WebAppProducer.Domain;
+
+namespace WebAppProducer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private ILogger<OrderController> _logger;
+
+        public OrderController(ILogger<OrderController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Insert(Order order)
+        {
+            try
+            {
+                #region Set in Queue
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(
+                        queue: "order_queue",
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    string message = JsonSerializer.Serialize(order);
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    var properties = channel.CreateBasicProperties();
+                    properties.Persistent = true;
+
+                    channel.BasicPublish(
+                        exchange: "",
+                        routingKey: "order_queue",
+                        basicProperties: properties,
+                        body: body);
+                }
+                #endregion
+                return Accepted(order);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error while trying to process a new order: {ex.Message}");
+                return BadRequest(ex);
+            }
+        }
+    }
+}
---
+++ Rabbit/WebAppProducer/Domain/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAppProducer.Domain
+{
+    public sealed class Order
+    {
+        public uint Id { get; set; }
+        public string Client { get; set; }
+        public IEnumerable<string> Items{ get; set; }
+    }
+}
